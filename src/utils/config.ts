bea4d55f21acba9f5fc7ed1a47f8a3fdb05462e7
Emmanuel Nwankwo <ematech811@gmail.com>
2026-04-27 14:50:07 +0100

    package installation updated

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-export const ApiIPAddress = "http://10.143.163.155:5000/api/v1";
+export const ApiIPAddress = "owotracbackend-production.up.railway.app";
 
 // To set the Metro bundler host: setx /M REACT_NATIVE_PACKAGER_HOSTNAME 10.143.163.155
 export const API_BASE_URL = ApiIPAddress;
